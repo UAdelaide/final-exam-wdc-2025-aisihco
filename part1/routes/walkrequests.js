043fb2bc9443f23d592cd6d0843a6d5cd99aa584
aisihco <caoxxker@gmail.com>
2025-06-20 02:44:27 +0000

    [automatic commit] 2025-06-20 02:44:27 UTC 58.174.95.121

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -4,8 +4,12 @@ const db = require('../db');
 
 
 router.get('/open', async (req, res) => {
+    try {
         const [rows] = await db.query(`SELECT wreq.request_id, d.name AS dog_name, wreq.requested_time, wreq.duration_minutes, wreq.location, u.username AS owner_username FROM WalkRequests wreq JOIN Dogs d ON wreq.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wreq.status = 'open'`);
         res.json(rows);
+    } catch (err) {
+        res.status(400)
+    }
 });
 
 module.exports = router;
