e263c1a3b6707371dc5617f39f2895c5fc6b3249
aisihco <caoxxker@gmail.com>
2025-06-20 08:07:38 +0000

    [automatic commit] 2025-06-20 08:07:38 UTC 58.174.95.121

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -54,11 +54,7 @@ router.post('/login', async (req, res) => {
       username: rows[0].username,
       role: rows[0].role
     };
-    res.json({
-      message: 'Successful',
-      user: rows[0],
-      redirectTo: rows[0].role === 'owner' ? '/owner-dashboard.html' 
-    })
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
