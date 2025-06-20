6908caf0b6a89c871f5b020c82f342384e573a4e
aisihco <caoxxker@gmail.com>
2025-06-20 07:55:23 +0000

    [automatic commit] 2025-06-20 07:55:23 UTC 58.174.95.121

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
-    secret: 'secret-key',
+    secret: 'secret',
     resave: false,
     saveUninitialized: true,
     cookie: {
