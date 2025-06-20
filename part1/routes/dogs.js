f481856e8a0cab73860ebdaef8763777fe493100
aisihco <caoxxker@gmail.com>
2025-06-20 02:39:04 +0000

    [automatic commit] 2025-06-20 02:39:04 UTC ;; communications error to 208.67.222.222#53: timed out 58.174.95.121

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -4,7 +4,6 @@ const db = require('../db');
 const { render } = require('../app');
 
 router.get('/dogs', async (req, res) => {
-    const [rows] = await db.query('
-        SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user.id');
+    const [rows] = await db.query('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user.id');
         res.json(rows);
-})
+});
