565b08cd27fb239ef5324cfc012b6ccb6c1bf7be
aisihco <caoxxker@gmail.com>
2025-06-20 02:41:25 +0000

    [automatic commit] 2025-06-20 02:41:25 UTC 58.174.95.121

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -4,7 +4,7 @@ const db = require('../db');
 
 
 router.get('/open', async (req, res) => {
-    const [rows] = await db.query('SELECT wreq.request');
+    const [rows] = await db.query('SELECT wreq.request_id, d.name ');
         res.json(rows);
 });
 
