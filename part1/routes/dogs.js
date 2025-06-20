4ac2d7ffd43144ed8e3e986b7ac117733245855b
aisihco <caoxxker@gmail.com>
2025-06-20 02:49:08 +0000

    [automatic commit] 2025-06-20 02:49:08 UTC 58.174.95.121

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -5,7 +5,12 @@ const db = require('../db');
 
 router.get('/', async (req, res) => {
     try {
-        const [rows] = await db.query(`SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user.id`);
+        const [rows] = await db.query(`
+            SELECT d.name AS dog_name, d.size, u.username
+            AS owner_username
+            FROM Dogs d
+            JOIN Users u
+            ON d.owner_id = u.user.id`);
         res.json(rows);
     } catch (err) {
         res.status(500).json({error: 'Failed dogs'});
