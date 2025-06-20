2a1b4fb9f95dfb6de67b2e94381765c877885095
aisihco <caoxxker@gmail.com>
2025-06-20 09:10:45 +0000

    [automatic commit] 2025-06-20 09:10:45 UTC 58.174.95.121

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,7 +43,7 @@ router.post('/login', async (req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, email, role FROM Users
       WHERE username = ? AND password_hash = ?
-    `, [username, password]);
+    `, [username, password]); // 
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
