261cc9e544089a839deab3a7c462c60d5960e35a
aisihco <caoxxker@gmail.com>
2025-06-20 03:20:35 +0000

    [automatic commit] 2025-06-20 03:20:35 UTC 58.174.95.121

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -9,7 +9,8 @@ router.get('/summary', async (req, res) => {
             SELECT u.username AS walker_username,
             COUNT(wrate.rating_id) AS total_ratings,
             ROUND(AVG(wrate.rating), 1) AS average_rating,
-            (SELECT COUNT(*) FROM WalkApplications wapp JOIN WalkRequests wreq ON wapp.request_id`);
+            (SELECT COUNT(*) FROM WalkApplications wapp JOIN WalkRequests wreq ON wapp.request_id = wreq.request_id
+            WHERE `);
         res.json(rows);
     } catch (err) {
         res.status(500).json({error: 'Failed dogs'});
