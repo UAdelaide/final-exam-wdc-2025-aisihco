251329661885fd8effd8cb0a14f7915f8bb48320
aisihco <caoxxker@gmail.com>
2025-06-20 08:45:01 +0000

    [automatic commit] 2025-06-20 08:45:01 UTC ;; communications error to 208.67.222.222#53: timed out 58.174.95.121

--- part2/app.js
+++ part2/app.js
@@ -14,8 +14,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'secret',
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        secure: false
+    cookie: { secure: false
     }
 }));
 
