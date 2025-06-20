51f12103defe3964678780d0322eaeef51896293
aisihco <caoxxker@gmail.com>
2025-06-20 02:35:58 +0000

    [automatic commit] 2025-06-20 02:35:58 UTC 58.174.95.121

--- part1/app.js
+++ part1/app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api')
+app.use('')
+app.use('/api/walkers', walkersRouter);
 
 module.exports = app;
