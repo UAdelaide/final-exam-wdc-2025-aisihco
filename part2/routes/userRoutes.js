f466547aa9fd912022bfc0367320ad8f03964390
aisihco <caoxxker@gmail.com>
2025-06-20 11:32:22 +0000

    [automatic commit] 2025-06-20 11:32:22 UTC 58.174.95.121

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 router.get('/dogs', async (req, res) => {
   const ownerID = req.session.user.user_id;
   try {
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?');
+    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerID]);
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch Dogs' });
