eeb0720d3e1d3f99296a9bc33ba4db9b78b0580b
aisihco <caoxxker@gmail.com>
2025-06-20 02:37:59 +0000

    [automatic commit] 2025-06-20 02:37:59 UTC 58.174.95.121

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -4,5 +4,6 @@ const db = require('../db');
 
 router.get('/dogs', async (req, res) => {
     const [rows] = await db.query('
-        SELECT d.name AS dog_name, d.size, u.username')
+        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        FROM Dogs d')
 })
