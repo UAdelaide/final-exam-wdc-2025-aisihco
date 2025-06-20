53f8bccc31fc28ad3010623b6cef7e8ba0a28ec8
aisihco <caoxxker@gmail.com>
2025-06-20 02:46:07 +0000

    [automatic commit] 2025-06-20 02:46:07 UTC 58.174.95.121

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const db = require('../db');
 
 
-router.get('/', async (req, res) => {
-    const [rows] = await db.query('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user.id');
+router.get('/summary', async (req, res) => {
+    try {
+        const [rows] = await db.query(`SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user.id`);
         res.json(rows);
+    } catch (err) {
+        res.status(500).json({error: 'Failed dogs'});
+    }
 });
 
 module.exports = router;
