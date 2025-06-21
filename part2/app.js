c949e59a8cf1c9f4cdb538afea63e6eb537f43a8
aisihco <caoxxker@gmail.com>
2025-06-21 00:43:54 +0000

    [automatic commit] 2025-06-21 00:43:54 UTC 58.174.95.121

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const db = require('../models/db');
+const db = require('./models/db');
 
 const app = express();
 
