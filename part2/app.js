20181499a25959e50ea98b725060f81aceb099ba
aisihco <caoxxker@gmail.com>
2025-06-21 00:38:53 +0000

    [automatic commit] 2025-06-21 00:38:53 UTC 58.174.95.121

--- part2/app.js
+++ part2/app.js
@@ -16,6 +16,25 @@ app.use(session({
     cookie: { secure: false }
 }));
 
+const db = require('../db');
+
+
+router.get('/', async (req, res) => {
+    try {
+        // getting the dog's name, size, and the user's username as owner username
+        // from the Dogs and Users table
+        const [rows] = await db.query(`
+            SELECT d.name AS dog_name, d.size, u.username
+            AS owner_username
+            FROM Dogs d
+            JOIN Users u
+            ON d.owner_id = u.user_id`);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed dogs' });
+    }
+});
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
